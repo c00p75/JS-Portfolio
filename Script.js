d75a6cb62540a8deac76ded02f31f33d9a97d042
c00p75_ <georgecoopmsapenda@gmail.com>
2022-11-16 13:44:06 +0200

    Add portifolio mobile menu

---
+++ Script.js
@@ -0,0 +1,21 @@
+let expandedNav = document.querySelector('.menu-btn');
+let navLinks =document.querySelector('#desktop_nav');
+
+expandedNav.addEventListener('click', function (event) {  
+  expandedNav.classList.toggle('active');
+  expandedNav.classList.toggle('active-menu-btn');
+  navLinks.classList.toggle('hide');
+  navLinks.classList.toggle('mobile-menu');
+  
+})
+
+navLinks.addEventListener('click', function (event) {  
+  if(event.target.className == 'hover_effect' && navLinks.classList.contains('mobile-menu')){
+    expandedNav.classList.toggle('active');
+    expandedNav.classList.toggle('active-menu-btn');
+    navLinks.classList.toggle('hide');    
+    navLinks.classList.toggle('mobile-menu');
+  }
+})
+
+console.log(navLinks.classList.contains('mobile-menu')); 
