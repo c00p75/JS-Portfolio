5908b6ae12eadbaaf6877020b8f67209d3eb6e34
c00p75_ <georgecoopmsapenda@gmail.com>
2022-11-16 14:02:56 +0200

    fix ESlint errors

--- Script.js
+++ Script.js
@@ -1,19 +1,18 @@
-let expandedNav = document.querySelector('.menu-btn');
-let navLinks =document.querySelector('#desktop_nav');
+const expandedNav = document.querySelector('.menu-btn');
+const navLinks = document.querySelector('#desktop_nav');
 
-expandedNav.addEventListener('click', function (event) {  
+expandedNav.addEventListener('click', ()=> {
   expandedNav.classList.toggle('active');
   expandedNav.classList.toggle('active-menu-btn');
   navLinks.classList.toggle('hide');
   navLinks.classList.toggle('mobile-menu');
-  
-})
+});
 
-navLinks.addEventListener('click', function (event) {  
-  if(event.target.className == 'hover_effect' && navLinks.classList.contains('mobile-menu')){
+navLinks.addEventListener('click', (event)=> {
+  if (event.target.className === 'hover_effect' && event.classList.contains('mobile-menu')){
     expandedNav.classList.toggle('active');
     expandedNav.classList.toggle('active-menu-btn');
-    navLinks.classList.toggle('hide');    
+    navLinks.classList.toggle('hide');
     navLinks.classList.toggle('mobile-menu');
   }
-})
+});
